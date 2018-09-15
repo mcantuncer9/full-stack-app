efbe813b6788da89a896928f91f7893ac4d50929
Mahmut Can Tuncer <mcantuncerr@gmail.com>
2018-09-15 23:54:08 +0300

    Now you can send message and that makes POST request to server

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,7 @@
 <template>
   <div @submit.prevent="addMessage">
-    <ul class="list-unstyled">
-      <li class="media" v-for="message in messages" :key="message._id">
-        <img v-if="message.imageURL" class="mr-3" :src="message.imageURL" :alt="message.subject">
-        <div class="media-body">
-          <h4 class="mt-0 mb-1">{{message.username}}</h4>
-          <h5 class="mt-0 mb-1">{{message.subject}}</h5>
-          <p>{{message.message}} <br /></p>
-          <small> {{message.created}} </small>
-          <hr>
-        </div>
-      </li>
-    </ul>
-    <form>
+    <button @click="showMessageForm = !showMessageForm" type="button" class="btn btn-info mt-3">Toggle Message Form</button>
+    <form v-if="showMessageForm">
       <div class="form-group">
         <label for="username">Username</label>
         <input v-model="message.username" type="text" class="form-control" id="username">
@@ -31,8 +20,20 @@
         <label for="imageURL">ImageURL</label>
         <input type="text" v-model="message.imageURL" class="form-control" id="imageURL" placeholder="Enter an image URL">
       </div>
-      <button type="submit" class="btn btn-success">Send Message!</button>
+      <button type="submit" class="btn btn-success mb-3">Send Message!</button>
     </form>
+    <ul class="list-unstyled mt-3">
+      <li class="media" v-for="message in reversedMessages" :key="message._id">
+        <img v-if="message.imageURL" class="mr-3" :src="message.imageURL" :alt="message.subject">
+        <div class="media-body">
+          <h4 class="mt-0 mb-1">{{message.username}}</h4>
+          <h5 class="mt-0 mb-1">{{message.subject}}</h5>
+          <p>{{message.message}} <br /></p>
+          <small> {{message.created}} </small>
+          <hr>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -43,18 +44,24 @@ const API_URL = 'http://localhost:1234/messages';
 export default {
   name: 'home',
   data: () => ({
+    showMessageForm: false,
     messages: [],
     message: {
       username: 'Anonymous',
       subject: '',
       message: '',
       imageURL: '',
-    }
+    },
   }),
   mounted() {
     fetch(API_URL).then(response => response.json()).then((result) => {
       this.messages = result;
     });
+  },
+  computed: {
+    reversedMessages(){
+      return this.messages.slice().reverse();
+    },
   },
   methods: {
     addMessage: function () {
@@ -62,9 +69,11 @@ export default {
         method: 'POST',
         body: JSON.stringify(this.message),
         headers: {
-          'content-type': 'application/json'
-        }
-      })
+          'content-type': 'application/json',
+        },
+      }).then(response => response.json()).then((result) => {
+        this.messages.push(result);
+      });
     },
   },
 };
